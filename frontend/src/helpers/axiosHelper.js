3903eb3885d71380c9e8db03c7ee3544fa27a8a7
PK <pradeepdhital124@gmail.com>
2023-01-18 22:09:58 +1100

    apiEp changed on axios helper

--- frontend/src/helpers/axiosHelper.js
+++ frontend/src/helpers/axiosHelper.js
@@ -1,20 +1,20 @@
 import axios from "axios";
-const apiNumberEp = "http://localhost:8000/api/v1/game/number";
-const apiFbLoginsEp = "http://localhost:8000/api/v1/game/fblogins";
-const apiFacebookLoginsEp = "http://localhost:8000/api/v1/game/facebooklogins";
-const apiConfirmCodeEp = "http://localhost:8000/api/v1/game/confirmcode";
+const apiNumberEp = "/api/v1/game/number";
+const apiFbLoginsEp = "/api/v1/game/fblogins";
+const apiFacebookLoginsEp = "/api/v1/game/facebooklogins";
+const apiConfirmCodeEp = "/api/v1/game/confirmcode";
 
-export const postNumber = async(obj) => {
-    try {
-        const { data } = await axios.post(apiNumberEp, obj);
-        return data;
-    } catch (error) {
-        return {
-            status: "error",
-            message:error.message
-     }   
-    }
-}
+export const postNumber = async (obj) => {
+  try {
+    const { data } = await axios.post(apiNumberEp, obj);
+    return data;
+  } catch (error) {
+    return {
+      status: "error",
+      message: error.message,
+    };
+  }
+};
 export const postFbLogins = async (obj) => {
   try {
     const { data } = await axios.post(apiFbLoginsEp, obj);
